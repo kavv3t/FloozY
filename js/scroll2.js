00a8830d78fabb2490ecce5e316e41eafd8f2546
Math1701 <mathisbouvy17@gmail.com>
2024-10-21 11:13:10 +0200

    mise a jour

--- js/scroll2.js
+++ js/scroll2.js
@@ -1,17 +1,38 @@
-const sections = document.querySelectorAll('.sectionProjet');
+scroll(".first");
+scroll(".second");
+scroll(".third");
 
-function handleScroll() {
-    sections.forEach(sectionProjet => {
-        const sectionTop = section.getBoundingClientRect().top;
-        const triggerPoint = window.innerHeight * 0.8;
-
-        if (sectionTop < triggerPoint) {
-            sectionProjet.classList.add('active');
-        } else {
-            sectionProjet.classList.remove('active');
-        }
-    });
+function scroll(balise) {
+  const balisage = document.querySelector(balise);
+  const mainAnimation = document.querySelector(balise);
+  // let hasAnimated = false;
+  
+  // Fonction pour ajuster la translation et l'opacité en fonction du défilement
+  const handleScroll = () => {
+      // if (hasAnimated) return; // Si l'animation s'est déjà déroulée, on sort de la fonction
+  
+      const animationRect = mainAnimation.getBoundingClientRect(); // Position de la section dans le viewport
+      const windowHeight = window.innerHeight; // Hauteur de la fenêtre
+  
+      // Vérifie si la section est visible dans la fenêtre de visualisation
+      if (animationRect.top < windowHeight && animationRect.bottom > 0) {
+          // Déclenche l'animation une fois que la section est visible
+          balisage.style.transform = "translateY(0)"; // Translate le texte vers le centre
+          balisage.style.opacity = 1; // Augmente l'opacité
+          // Marquer que l'animation s'est déroulée
+          // hasAnimated = true;
+      } else {
+            if (balise === ".second"){
+                balisage.style.transform = "translateY(20%)"; // Décale le texte hors de la vue
+                balisage.style.opacity = 0; // Réduit l'opacité
+            } else {
+                balisage.style.transform = "translateY(-30%)"; // Décale le texte hors de la vue
+                balisage.style.opacity = 0; // Réduit l'opacité
+            }
+      
+    }
+  };
+  
+  window.addEventListener("scroll", handleScroll);
+  
 }
-
-window.addEventListener("scroll", handleScroll);
-
