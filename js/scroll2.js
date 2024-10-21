8d1afdfd660fe6ba025167f14609f09ed33e4724
Math1701 <mathisbouvy17@gmail.com>
2024-10-21 09:51:51 +0200

    mise a jour

--- js/scroll2.js
+++ js/scroll2.js
@@ -1,14 +1,17 @@
+const sections = document.querySelectorAll('.section');
 
-    const sections = document.querySelectorAll('.section');
+function handleScroll() {
     sections.forEach(section => {
         const sectionTop = section.getBoundingClientRect().top;
         const triggerPoint = window.innerHeight * 0.8;
 
         if (sectionTop < triggerPoint) {
             section.classList.add('active');
-        }else {
+        } else {
             section.classList.remove('active');
         }
     });
+}
 
+window.addEventListener("scroll", handleScroll);
 
