68c2bddf133b31a285e8f64a9f37f25128e2861f
kavv3t <lamafofor@gmail.com>
2024-10-21 11:13:42 +0200

    modif scroll3 et reponsive

--- js/scroll3.js
+++ js/scroll3.js
@@ -5,11 +5,11 @@ scroll(".third");
 function scroll(balise) {
   const balisage = document.querySelector(balise);
   const mainAnimation = document.querySelector(balise);
-  let hasAnimated = false; // Flag pour vérifier si l'animation s'est déjà déroulée
+  // let hasAnimated = false;
   
   // Fonction pour ajuster la translation et l'opacité en fonction du défilement
   const handleScroll = () => {
-      if (hasAnimated) return; // Si l'animation s'est déjà déroulée, on sort de la fonction
+      // if (hasAnimated) return; // Si l'animation s'est déjà déroulée, on sort de la fonction
   
       const animationRect = mainAnimation.getBoundingClientRect(); // Position de la section dans le viewport
       const windowHeight = window.innerHeight; // Hauteur de la fenêtre
@@ -20,8 +20,12 @@ function scroll(balise) {
           balisage.style.transform = "translateX(0)"; // Translate le texte vers le centre
           balisage.style.opacity = 1; // Augmente l'opacité
           // Marquer que l'animation s'est déroulée
-          hasAnimated = true;
-      }   
+          // hasAnimated = true;
+      } else {
+        // Si l'élément n'est plus visible, on réinitialise les styles
+        balisage.style.transform = "translateX(-100%)"; // Décale le texte hors de la vue
+        balisage.style.opacity = 0; // Réduit l'opacité
+    }
   };
   
   window.addEventListener("scroll", handleScroll);
