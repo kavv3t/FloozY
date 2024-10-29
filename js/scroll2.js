21f07402b9b50937fbcf55efd986c5c48bbc64e1
kavv3t <lamafofor@gmail.com>
2024-10-29 22:11:30 +0100

    better

--- js/scroll2.js
+++ js/scroll2.js
@@ -1,7 +1,7 @@
 scroll(".first");
 scroll(".second");
 scroll(".third");
-scroll(".partie2");
+scroll(".partie3");
 
 function scroll(balise) {
   const balisage = document.querySelector(balise);
@@ -22,14 +22,14 @@ function scroll(balise) {
           balisage.style.opacity = 1; // Augmente l'opacité
           // Marquer que l'animation s'est déroulée
           // hasAnimated = true;
-      } else if (balise === ".partie2" && animationRect.top < windowHeight + 200) { // && animationRect.bottom > 200 ne fonctionne pas apparemment
+      } else if (balise === ".partie3" && animationRect.top < windowHeight + 200) { // && animationRect.bottom > 200 ne fonctionne pas apparemment
         balisage.style.transform = "translateY(0)"; // Translate le texte vers le centre
         balisage.style.opacity = 1; 
       } else {
             if (balise === ".second"){
                 balisage.style.transform = "translateY(20%)"; // Décale le texte hors de la vue
                 balisage.style.opacity = 0; // Réduit l'opacité
-            } else if (balise === ".partie2") {
+            } else if (balise === ".partie3") {
                 balisage.style.transform = "translateY(50%)";
                 balisage.style.opacity = 0;
             } else {
